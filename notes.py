27546a8cbd35321b5c1410520a7e67b37b03f6ee
michellezh <michellezh2002@gmail.com>
2019-06-24 14:24:58 -0400

    init commit

---
+++ notes.py
@@ -0,0 +1,48 @@
+#myvar = "python"
+#print(myvar)
+
+#myvar = 42
+#print(myvar)
+
+#mystr = "This is my string! It's cool!"
+#mystr2 = 
+#print (mystr)
+#print(mystr2)
+
+##Conditionals
+#if True:
+    #print("This is true")
+#if False:
+    #print("THe if is true")
+#else:
+    #print("No the else is true!!!")
+
+#if False
+    #print("first")
+#elif False:
+    #print('second')
+#elif False:
+    #print('fourth')
+#elif:
+    #print("last")
+
+#i=0
+#while i < 10:
+    #print('hi there')
+    #i = i + 1
+    #i += 1 equals i = i + 1
+#for x in range(1, 6):
+    #print(x ** 2)
+#mylist = [1, 2, 3, 42, True, "a string", 3.14, [4,5,6]]
+#grocery_list = ['bananas', 'apples', 'ice cream', 'milk']
+
+#print(grocery_list[2])
+
+#first_item = mylist[0] = 'chips'
+##print "clear to erase"
+
+#mylist_length = len(mylist)
+
+#print(mylist_length)
+#print(len([1, 2, 3, ]))
+
